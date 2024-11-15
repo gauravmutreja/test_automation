1919ced651144127af9043afa0f5475cd46b2263
Gaurav Mutreja <gauravmutreja@gaurav-MacBook-Air.fritz.box>
2024-11-15 15:52:12 +0100

    Added a new test

---
+++ src/test/java/practice_tests/Login_Test.java
@@ -0,0 +1,28 @@
+package practice_tests;
+
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Login_Test {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("Test");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.cssSelector("[type='submit']")).click();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Log Out']")));
+        System.out.println(driver.findElement(By.tagName("h1")).getText());
+
+        driver.quit();
+    }
+}
