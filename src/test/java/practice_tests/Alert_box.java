d46fcfa9d892e26c39cf61fcdb9f7e292074690b
Gaurav Mutreja <gauravmutreja@gaurav-MacBook-Air.fritz.box>
2024-11-26 21:55:54 +0100

    Fixed comment

--- src/test/java/practice_tests/Alert_box.java
+++ src/test/java/practice_tests/Alert_box.java
@@ -1,7 +1,9 @@
 package practice_tests;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -15,11 +17,12 @@ public class Alert_box {
             driver.get("https://rahulshettyacademy.com/AutomationPractice");
             driver.findElement(By.cssSelector("#name")).sendKeys("Gaurav M");
             driver.findElement(By.cssSelector("#alertbtn")).click();
-            System.out.println(driver.switchTo().alert().getText());
-            driver.switchTo().alert().accept();
+            Alert alert = driver.switchTo().alert();
+            System.out.println(alert.getText());
+            alert.accept();
             driver.findElement(By.xpath("//*[@value='Confirm']")).click();
-            System.out.println(driver.switchTo().alert().getText());
-            driver.switchTo().alert().dismiss();
+            System.out.println(alert.getText());
+            alert.dismiss();
         } finally {
             driver.quit();
         }
